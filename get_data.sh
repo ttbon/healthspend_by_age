01c248b077e578a1752774afbd091a40fd4a52fe
epimentel6 <eap.pimentel@gatech.edu>
2023-01-23 13:03:27 -0500

    prepare README

--- get_data.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-mkdir data/input
-cd data/input
-
-# medical inflation data
-curl -O https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1169&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=CPIMEDSL&scale=left&cosd=1947-01-01&coed=2022-12-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2023-01-12&revision_date=2023-01-12&nd=1947-01-01
